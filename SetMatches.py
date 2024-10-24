d2dba056739a0cbb9f4b0ec1fcbce1c5bf34372e
Matteus Öhrberg <matteus.ohrberg@learnet.se>
2024-10-24 12:05:10 +0200

    Just a todo list so far, will be used for setmatches mode

---
+++ SetMatches.py
@@ -0,0 +1,9 @@
+import time
+import random
+import os
+import playerCombat
+import SpecializationPick
+import colorama
+
+#TODO: 10 fights in increasing difficulty, increasing player HP, better descriptions(?), multi-combat (animals), selfID for descriptions?
+#Bravery points? Shields???
